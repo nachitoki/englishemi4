b6674fb7c211b34e70c2b5c7d5b62a9646858e2f
nachitoki <cibeltrancarvajal@gmail.com>
2025-08-22 14:40:25 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -9,3 +9,5 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
+
+import './styles.css'
