59c71ba227580acea4c26f0f7ef04059f4419f83
nachitoki <cibeltrancarvajal@gmail.com>
2025-08-22 18:17:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/englishemi4/',
+  // usar rutas relativas hace que funcione bajo /englishemi4/ y también si cambias el nombre del repo
+  base: './',
   plugins: [react()],
-});
+})
